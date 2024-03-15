6ff936dfdec2091eaada282455d725729b4289ac
chrelyonly <1172576293@qq.com>
2024-03-15 16:04:48 +0800

    1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import "./src/main.js";
+// import express from "express";
+//
+// const app = express();
+// app.get('/test', (req, res) => {
+//     res.send('hello world')
+// });
+// app.listen(3000, () => {
+//     console.log('Server started on port 3000')
+// })
