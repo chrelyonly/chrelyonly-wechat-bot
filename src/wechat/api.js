c0458f61ad23e7080fe2de68555890dcff247afb
chrelyonly <1172576293@qq.com>
2024-04-07 14:18:21 +0800

    1

--- src/wechat/api.js
+++ src/wechat/api.js
@@ -75,6 +75,21 @@ export async  function onMessage(message,bot) {
                     text: msg
                 }
                 setCache(message.id,JSON.stringify(cacheJson))
+
+                // 如果缓存包含测试字符串则发送视频
+                // if (msg.toString().includes("来点视频")){
+                //     // 生成1到10之间的随机整数
+                //     let randomNumber = Math.floor(Math.random() * 10) + 1;
+                //     const fileBox = FileBox.fromFile("C:\\Users\\chrelyonly\\Downloads\\testqqq\\" + randomNumber + ".jpg")
+                //     room.say(fileBox)
+                // }
+                //
+                // if (msg.toString().includes("来点真视频")){
+                //     // 生成1到10之间的随机整数
+                //     let randomNumber = Math.floor(Math.random() * 4) + 1 + 10;
+                //     const fileBox = FileBox.fromFile("C:\\Users\\chrelyonly\\Downloads\\testqqq\\" + randomNumber + ".mp4")
+                //     room.say(fileBox)
+                // }
                 //     执行复读机,复读机只复读文字消息
                 // room.say(message.text() + "\n-测试")
             }
