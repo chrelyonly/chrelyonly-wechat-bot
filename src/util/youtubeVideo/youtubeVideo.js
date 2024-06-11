298c1fb2271b4afc2fc15817d9ccde52cc74ca54
chrelyonly <1172576293@qq.com>
2024-06-11 17:06:35 +0800

    更改

--- src/util/youtubeVideo/youtubeVideo.js
+++ src/util/youtubeVideo/youtubeVideo.js
@@ -5,15 +5,15 @@ import {log} from "wechaty";
 
 export const youtubeVideo = (talker,text,room,bot)=>{
 //     进行视频解析
-    let api = "https://tubedown.cn/api/youtube";
+    let api = "https://cdn36.savetube.me";
     let params = {
         url: text,
     }
     let headers = {
         // "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
     }
-    http(api,"post",params, 2,headers).then( res=> {
-        let fileBox = FileBox.fromUrl(res.data.data.formats[0].url, {name:"oduyin.mp4"});
+    http(api,"get",params, 1,headers).then( res=> {
+        let fileBox = FileBox.fromUrl(res.data.data.video_formats[0].url, {name:"youtube.mp4"});
         room.say("解析成功:" + res.data.data.title)
         room.say(fileBox)
     },err =>{
