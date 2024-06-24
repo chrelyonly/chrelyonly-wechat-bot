6ab9653be2c8c15c1e78a90f666a29c835a9ccee
chrelyonly <1172576293@qq.com>
2024-06-24 11:38:03 +0800

    更改

--- src/mqtt/mqttMain.js
+++ src/mqtt/mqttMain.js
@@ -1,22 +1,27 @@
-export const SOCKET_SERVICE = ' wss://temp-img.chrelyonly.cn/mqtt'
+import {mqttMessage,userId} from "./mqttUtil.js";
+
+export const SOCKET_SERVICE = ' wss://chrelyonly.cn/mqtt'
 export const USERNAME = "chrelyonly";
 export const PASSWORD = "chrelyonly";
 // 引入全局websocket
 import mqtt from "mqtt";
 // 服务端mqtt消息
 let mqttServer = null;
-// 机器人
-let bot = null;
-try {
-    // 这里自行取一个名字id
-    let userId = new Date().Format("yyyyMMddhhmmssSSS");
-    // 用户订阅id
-    const singleTopic = "/userId" + userId;
-    // 在线人数订阅
-    const onlineUserNum = "/onlineUserNum";
-    // 能量订阅
-    const wechatRun = "/wechatRun"
-
+// 用户订阅id
+const singleTopic = "/userId" + userId;
+// 在线人数订阅
+const onlineUserNum = "/onlineUserNum" + userId;
+// 能量订阅
+const wechatRun = "/wechatRun" + userId
+/**
+ * 初始化mqtt
+ * @param bot
+ */
+export const mqttMain = (bot) => {
+    mqttServer = mqtt.connect(SOCKET_SERVICE, {
+        username: USERNAME,
+        password: PASSWORD
+    });
     //监听连接状态
     mqttServer.on("connect", () => {
         console.log("连接成功");
@@ -38,19 +43,5 @@ try {
     });
     mqttServer.on("message", (topic, message) => {
         mqttMessage(topic, message, singleTopic, bot)
-    });
-} catch (e) {
-    console.log(e);
-    console.log("mqtt连接失败");
-}
-
-/**
- * 初始化mqtt
- * @param bot
- */
-export const mqttMain = (bot) => {
-    mqttServer = mqtt.connect(SOCKET_SERVICE, {
-        username: USERNAME,
-        password: PASSWORD
     });
 }
--- src/mqtt/mqttUtil.js
+++ src/mqtt/mqttUtil.js
@@ -1,19 +1,35 @@
 /**
+ * 这里自行取一个名字id
+ * 我的id
+ */
+export const userId = new Date().Format("chrelyonly");
+/**
+ * 机器人id list 用于发消息时判断是否是机器人,防止机器人互相发消息
+ * 集群id 从远端获取
+ * @type {*[]}
+ */
+export const botList = []
+
+/**
  * mqtt消息
  * @param topic 订阅
  * @param message 消息
  * @param userId 我自己id
  * @param bot 机器人
  */
-const mqttMessage = (topic, message, userId, bot) => {
-    if (topic === "/onlineUserNum") {
-        console.log("在线人数:" + message.toString());
-        msgUtil(bot,"当前在线人数:" + message.toString())
+export const mqttMessage = (topic, message, userId, bot) => {
+    if (topic.includes("/onlineUserNum" + userId)) {
+        let userInfo = JSON.parse(message.toString())
+        msgUtil(bot,"当前在线人数:" + userInfo.count)
+    //   去重然后存入userInfo.id
+        if (!botList.includes(userInfo.id)) {
+            botList.push(userInfo.id)
+        }
     }
     if (topic.includes("/userId" + userId)) {
         msgUtil(bot,"收到其他机器人消息:" + message.toString())
     }
-    if (topic.includes("/wechatRun")) {
+    if (topic.includes("/wechatRun" + userId)) {
         msgUtil(bot,"wechatRun:" + message.toString())
     }
 }
