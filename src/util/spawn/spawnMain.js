3345040d0bc0bcbbbd43f8d9e4c3b56e3a88af9b
chrelyonly <1172576293@qq.com>
2024-08-08 10:44:09 +0800

    1

--- src/util/spawn/spawnMain.js
+++ src/util/spawn/spawnMain.js
@@ -1,5 +1,5 @@
 import { spawn } from "child_process"
-import iconv from "iconv-lite"
+// import iconv from "iconv-lite"
 // 先这样用 ,后面改到web端控制,  多开在线登录控制.....
 
 
@@ -16,16 +16,16 @@ export const getPm2Info = (room,bot)=>{
 // 监听标准输出数据
     pm2.stdout.on('data', (data) => {
         // 使用 iconv-lite 将数据从 GBK 编码转换为 UTF-8
-        const output = iconv.decode(data, 'gbk');
-        console.log(`标准输出:\n${output}`);
-        room.say(output)
+        // const output = iconv.decode(data, 'gbk');
+        // console.log(`标准输出:\n${output}`);
+        room.say(data)
     });
 
 // 监听标准错误数据
     pm2.stderr.on('data', (data) => {
         // 使用 iconv-lite 将数据从 GBK 编码转换为 UTF-8
-        const output = iconv.decode(data, 'gbk');
-        console.error(`错误输出:\n${output}`);
+        // const output = iconv.decode(data, 'gbk');
+        console.error(`错误输出:\n${data}`);
     });
 
 // 监听子进程关闭事件
