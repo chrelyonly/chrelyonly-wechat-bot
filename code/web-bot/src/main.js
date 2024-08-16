de0ce684c00c43b579ebe3513df29493d7a1085d
chrelyonly <1172576293@qq.com>
2024-08-16 13:49:38 +0800

    1

--- code/web-bot/src/main.js
+++ code/web-bot/src/main.js
@@ -7,8 +7,13 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+//需要先安装ElementPlus的依赖
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
+app.use(Avue);
 app.mount('#app')
