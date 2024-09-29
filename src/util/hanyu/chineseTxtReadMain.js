815d8c5088ce128136335d6b9f654ef36f5175eb
chrelyonly <1172576293@qq.com>
2024-09-29 12:36:55 +0800

    1

--- src/util/hanyu/chineseTxtReadMain.js
+++ src/util/hanyu/chineseTxtReadMain.js
@@ -8,13 +8,14 @@ const writeRegex = /https:\/\/hanyu-word-gif\.cdn\.bcebos\.com\/[^\s'"]+/;
 // 怎么读
 export const chineseTxtRead = (room,bot,message) => {
     let params = {
-        wd: message
+        wd: message,
+        ptype: "zici"
     }
     http("https://hanyu.baidu.com/s","get",params,1,{}).then( res => {
-        const match = res.data.match(readRegex);
+        const match = res.data.match(writeRegex);
         if (match) {
-            const fileBox = FileBox.fromUrl(match[0], {name:"chineseTxtRead" + message + new Date().Format("yyyy-MM-dd HH:mm:ss")});
-            room.say(fileBox)
+            const text = match[match.length-1].replace(/<[^>]*>/g, '').trim(); // 去掉任何HTML标签
+            room.say(text)
         }
     })
 }
@@ -22,13 +23,14 @@ export const chineseTxtRead = (room,bot,message) => {
 // 怎么写
 export const chineseTxtWrite = (room,bot,message) => {
     let params = {
-        wd: message
+        wd: message,
+        ptype: "zici"
     }
     http("https://hanyu.baidu.com/s","get",params,1,{}).then( res => {
-        const match = res.data.match(writeRegex);
+        const match = res.data.match(readRegex);
         if (match) {
-            const text = match[match.length-1].replace(/<[^>]*>/g, '').trim(); // 去掉任何HTML标签
-            room.say(text)
+            const fileBox = FileBox.fromUrl(match[0], {name:"chineseTxtRead" + message + new Date().Format("yyyy-MM-dd HH:mm:ss")});
+            room.say(fileBox)
         }
     })
 }
