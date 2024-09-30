f7fa8e37b535afe7a6f8ebc4e9b436b70947bba0
chrelyonly <1172576293@qq.com>
2024-09-30 11:06:41 +0800

    1

--- code/web-bot/vite.config.js
+++ code/web-bot/vite.config.js
@@ -3,20 +3,20 @@ import {fileURLToPath, URL} from 'node:url'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { compression } from 'vite-plugin-compression2'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    // 就是使用这个插件实现的文件压缩
-    compression({
-      threshold:2000, // 设置只有超过 2k 的文件才执行压缩
-      deleteOriginalAssets:true, // 设置是否删除原文件
-      skipIfLargerOrEqual:true, // 如果压缩后的文件大小与原文件大小一致或者更大时，不进行压缩
-      // 其他的属性暂不需要配置，使用默认即可
-    })
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
@@ -37,11 +37,12 @@ export default defineConfig({
   },
 
   build: {
+    minify: 'terser', // 使用 terser 压缩
     rollupOptions: {
       output: {
-        chunkFileNames: 'static/js/[name]-[hash].js',
-        entryFileNames: 'static/js/[name]-[hash].js',
-        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        // chunkFileNames: 'static/js/[name]-[hash].js',
+        // entryFileNames: 'static/js/[name]-[hash].js',
+        // assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[2].split('/')[0].toString();
@@ -55,6 +56,7 @@ export default defineConfig({
         drop_console: true, // 移除console
         drop_debugger: true, // 移除debugger
       },
+      sourceMap: false,
       mangle: {
         // 开启变量名混淆
         toplevel: true,
