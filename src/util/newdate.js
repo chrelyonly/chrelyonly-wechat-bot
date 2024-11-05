ad782b23b59a1b7be335a2da1ecf1f61547e55eb
chrelyonly <1172576293@qq.com>
2024-11-05 12:13:21 +0800

    1.完善日期格式化工具

--- src/util/newdate.js
+++ src/util/newdate.js
@@ -1,24 +1,37 @@
-export default // 对Date的扩展，将 Date 转化为指定格式的String
-// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
-// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+// 对 Date 的扩展，将 Date 转化为指定格式的 String
+// 月(M)、日(d 或 D)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+// 年(y 或 Y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
 // 例子：
-// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
-// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
-Date.prototype.Format = function (fmt) { //author: meizz
-    var o = {
-        "M+": this.getMonth() + 1,                 //月份
-        "d+": this.getDate(),                    //日
-        "h+": this.getHours(),                   //小时
-        "H+": this.getHours(),                   //小时
-        "m+": this.getMinutes(),                 //分
-        "s+": this.getSeconds(),                 //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds()             //毫秒
+// (new Date()).Format("YYYY-MM-DD hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+// (new Date()).Format("YYYY-M-D h:m:s.S")      ==> 2006-7-2 8:9:4.18
+
+Date.prototype.Format = function (fmt) {
+    const o = {
+        "M+": this.getMonth() + 1,                 // 月份
+        "d+": this.getDate(),                        // 日
+        "D+": this.getDate(),                        // 日 (大写)
+        "h+": this.getHours(),                       // 小时 (12小时制)
+        "H+": this.getHours(),                       // 小时 (24小时制)
+        "m+": this.getMinutes(),                     // 分
+        "s+": this.getSeconds(),                     // 秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
+        "S": this.getMilliseconds()                  // 毫秒
     };
-    if (/(y+)/.test(fmt))
-        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt))
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+
+    // 替换年份
+    if (/(Y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, String(this.getFullYear()).substr(4 - RegExp.$1.length));
+    } else if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, String(this.getFullYear()).substr(4 - RegExp.$1.length));
+    }
+
+    // 替换其他格式
+    for (const [key, value] of Object.entries(o)) {
+        const regex = new RegExp(`(${key})`);
+        if (regex.test(fmt)) {
+            fmt = fmt.replace(regex, (regex.exec(fmt)[0].length === 1) ? value : String(value).padStart(2, '0'));
+        }
+    }
+
     return fmt;
-}
+};
