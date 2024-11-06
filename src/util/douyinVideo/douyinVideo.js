11b252cc4e27399a033f0b369e8080bdc67bf8a7
chrelyonly <1172576293@qq.com>
2024-11-06 09:34:45 +0800

    1.完善日期格式化工具

--- src/util/douyinVideo/douyinVideo.js
+++ src/util/douyinVideo/douyinVideo.js
@@ -14,6 +14,7 @@ function extractUrl(text) {
 }
 
 export const douyinVideo = (talker, text, room, bot) => {
+    log.info("正在解析抖音视频......")
 //     进行视频解析
     let api = "https://api.pearktrue.cn/api/video/douyin/";
     let params = {
@@ -23,7 +24,10 @@ export const douyinVideo = (talker, text, room, bot) => {
     let headers = {
         url: urls[0]
     }
+    log.info(urls[0])
     http(api, "get", params, 1, headers).then(res => {
+        log.info("解析结果")
+        log.info(res)
         let fileBox = FileBox.fromUrl(res.data.data.url, {name: "oduyin.mp4"});
         room.say(fileBox)
     }, err => {
--- src/util/test/testMain.js
+++ src/util/test/testMain.js
@@ -6,7 +6,7 @@ function extractUrl(text) {
 }
 
 // 示例用法
-const inputText = "7.92 J@i.pD kPX:/ 01/17 第1～5集:虾仁穿越意123123123123外躺在棺中 # 原创动画 # 沙雕动画大合集 # 原创视频 # 原创动漫 # 沙雕剧情系列 https://v.douyin.com/iAL5b9aq/ 复制此链接，打开Dou音搜索，直接观看视频！";
+const inputText = "3.58 复制打开抖音，看看【爱莉爱莉爱的作品】爱莉翻唱⟪雾里⟫‶你在雾里，我看不清你″# 崩坏3... https://v.douyin.com/iALmU4AB/ 01/07 vFu:/ t@e.bN";
 const urls = extractUrl(inputText);
 
 console.log(urls[0]);  // 输出：["https://v.douyin.com/iAL5b9aq/"]
