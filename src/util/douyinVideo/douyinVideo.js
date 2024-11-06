437e08a668e5d2aebae41168e7341470439f9cea
chrelyonly <1172576293@qq.com>
2024-11-06 09:44:58 +0800

    1.完善日期格式化工具

--- src/util/douyinVideo/douyinVideo.js
+++ src/util/douyinVideo/douyinVideo.js
@@ -17,17 +17,13 @@ export const douyinVideo = (talker, text, room, bot) => {
     log.info("正在解析抖音视频......")
 //     进行视频解析
     let api = "https://api.pearktrue.cn/api/video/douyin/";
-    let params = {
-        pageUrl: text,
-    }
     const urls = extractUrl(text);
-    let headers = {
+    let params = {
         url: urls[0]
     }
-    log.info(urls[0])
+    let headers = {
+    }
     http(api, "get", params, 1, headers).then(res => {
-        log.info("解析结果")
-        log.info(JSON.stringify(res))
         let fileBox = FileBox.fromUrl(res.data.data.url, {name: "oduyin.mp4"});
         room.say(fileBox)
     }, err => {
