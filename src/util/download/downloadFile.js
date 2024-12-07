2450f4ea66d99c72b3401a94a3acc723a1c09258
chrelyonly <1172576293@qq.com>
2024-12-07 14:07:52 +0800

    1.移除一些东西

--- src/util/download/downloadFile.js
+++ src/util/download/downloadFile.js
@@ -13,7 +13,7 @@ const timestamp = () => new Date().Format("YYYYMMDDHHmmss");
 // 修改目标目录
 const STATIC_DIR = "/chrelyonly-wechat-bot/static/temp/download/";
 
-export const downloadFile = async (talker, text, room) => {
+export const downloadFile = (talker, text, room) => {
     log.info("尝试下载文件.....");
     try {
         const strings = text.toString().split("--");
@@ -25,31 +25,40 @@ export const downloadFile = async (talker, text, room) => {
         const proxy = proxyUrl ? new HttpsProxyAgent(proxyUrl) : undefined;
 
         // 下载文件
-        const res = await http(strings[1], "get", {}, 3, headers, proxy);
-        log.info("下载图片完成...");
+        http(strings[1], "get", {}, 3, headers, proxy).then(res=>{
+            log.info("下载图片完成...");
+            // 保存临时文件
+            const paths = `${timestamp()}temp`;
+            const tempDir = path.join(STATIC_DIR, paths);
+            fs.promises.mkdir(tempDir, { recursive: true }).then(()=>{
+                const paths1 = `${timestamp()}.${strings[2]}`;
+                const filePath = path.join(tempDir, paths1);
+                const fileBox = FileBox.fromBuffer(res.data, strings[2]);
+                fileBox.toFile(filePath).then(()=>{
+                    log.info("文件已保存:", filePath);
 
-        // 保存临时文件
-        const paths = `${timestamp()}temp`;
-        const tempDir = path.join(STATIC_DIR, paths);
-        await fs.promises.mkdir(tempDir, { recursive: true });
-        const paths1 = `${timestamp()}.${strings[2]}`;
-        const filePath = path.join(tempDir, paths1);
-        const fileBox = FileBox.fromBuffer(res.data, strings[2]);
-        await fileBox.toFile(filePath);
-        log.info("文件已保存:", filePath);
+                    // 压缩文件
+                    // const zipPath = path.join(STATIC_DIR, `${timestamp()}download.zip`);
+                    // await archiveFolder(tempDir, zipPath);
+                    // log.info("文件已压缩:", zipPath);
 
-        // 压缩文件
-        // const zipPath = path.join(STATIC_DIR, `${timestamp()}download.zip`);
-        // await archiveFolder(tempDir, zipPath);
-        // log.info("文件已压缩:", zipPath);
+                    // 发送文件
+                    log.info("文件已转存地址1: 点击下载 https://bot-api.dj.non-human-research-center.top/temp/download/" + paths + "/" + paths1);
+                    room.say("文件已转存地址1: 点击下载 https://bot-api.dj.non-human-research-center.top/temp/download/" + paths + "/" + paths1);
 
-        // 发送文件
-        log.info("文件已转存地址1: 点击下载 https://bot-api.dj.non-human-research-center.top/temp/download/" + paths + "/" + paths1);
-        room.say("文件已转存地址1: 点击下载 https://bot-api.dj.non-human-research-center.top/temp/download/" + paths + "/" + paths1);
-        log.info("文件已转存地址2: 点击下载 https://web-bot.frp.chrelyonly.cn/temp/download/" + paths + "/" + paths1);
-        room.say("文件已转存地址2: 点击下载 https://web-bot.frp.chrelyonly.cn/temp/download/" + paths + "/" + paths1);
-        const fileBoxZip = FileBox.fromFile(filePath);
-        await room.say(fileBoxZip);
+                    setTimeout(()=>{
+                        log.info("文件已转存地址2: 点击下载 https://web-bot.frp.chrelyonly.cn/temp/download/" + paths + "/" + paths1);
+                        room.say("文件已转存地址2: 点击下载 https://web-bot.frp.chrelyonly.cn/temp/download/" + paths + "/" + paths1);
+                    },1000)
+
+                    setTimeout(() => {
+                        const fileBoxZip = FileBox.fromFile(filePath);
+                        room.say(fileBoxZip);
+                    },3000)
+                });
+            });
+        });
+
 
     } catch (e) {
         log.error("处理过程中发生错误:", e);
